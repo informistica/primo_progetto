eeb4827e2484a96e7c159551afcbcb2d2756281b
informistica <mauro.spinarelli@gmail.com>
2020-09-30 16:53:16 +0200

    Update views.py

--- prima_app/views.py
+++ prima_app/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 def homepage(request):
     return HttpResponse("<h1>Ciao a tutti! Benvenuti nella homepage!</h1>")
 
-#L2
+#L3
 def menu(request):
     return HttpResponse("<ol><li>Prima opzione</li><li>Seconda opzione</li><li>Terza opzione</li></ol>")

baeb30c4630a03ed2da2e6bae8739f6f0b949c98
informistica <mauro.spinarelli@gmail.com>
2020-11-03 22:53:38 +0100

    ...

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, ArticoloDetailViewCB #articoloDetailView
+from .views import home, ArticoloDetailViewCB, ArticoloListView #articoloDetailView
 urlpatterns = [
     path("", home, name="homeview"),
     #path("articoli/<int:pk>", articoloDetailView, name="articolo_detail")
     path("articoli/<int:pk>", ArticoloDetailViewCB.as_view(), name="articolo_detail"),
+    path("lista_articoli/", ArticoloListView.as_view(), name="lista_articoli")
 ]
 

8bf788294d1668ad02c6604252bf98ffafbc86fc
informistica <mauro.spinarelli@gmail.com>
2020-11-03 16:48:05 +0100

    Update models.py

--- news/models.py
+++ news/models.py
@@ -1,11 +1,8 @@
 from django.db import models
-from django.urls import reverse
 
 # I modelli sono sottoclassi di django.db.models.Model
 # I campi dei modelli sono invece definiti in django.db.models.fields
 # Campi dei Modelli: https://docs.djangoproject.com/en/3.0/ref/models/fields/
-# ForeignKey: https://docs.djangoproject.com/en/3.0/ref/models/fields/#django.db.models.ForeignKey
-# Making Queries: https://docs.djangoproject.com/en/3.0/topics/db/queries/
 
 # Create your models here.
 
@@ -21,4 +18,3 @@ class Articolo(models.Model):
     """ il modello generico di un articolo di news """
     titolo = models.CharField(max_length=100) # alcuni campi necessitano di parametri obbligatori!
     contenuto = models.TextField()
-   

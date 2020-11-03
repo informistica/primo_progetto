65690c6eb0971f8053359c6f043e921a7a108af0
informistica <mauro.spinarelli@gmail.com>
2020-11-03 22:39:58 +0100

    ...

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, articoloDetailView
+from .views import home, ArticoloDetailViewCB #articoloDetailView
 urlpatterns = [
     path("", home, name="homeview"),
-    path("articoli/<int:pk>", articoloDetailView, name="articolo_detail")
+    #path("articoli/<int:pk>", articoloDetailView, name="articolo_detail")
+    path("articoli/<int:pk>", ArticoloDetailViewCB.as_view(), name="articolo_detail"),
 ]
 
--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Articolo, Giornalista
+from django.views.generic.detail import DetailView
 # Create your views here.
 def home(request):
     articoli = Articolo.objects.all()
@@ -8,10 +9,13 @@ def home(request):
     context = {"articoli": articoli, "giornalisti": giornalisti}
     print(context)
     return render(request, "homepage.html", context)
-
 def articoloDetailView(request, pk):
      # articolo = Articolo.objects.get(pk=pk)
      articolo = get_object_or_404(Articolo, pk=pk)
      context = {"articolo": articolo}
      return render(request, "articolo_detail.html", context)
+
+class ArticoloDetailViewCB(DetailView):
+    model = Articolo
+    template_name = "articolo_detail.html"
 

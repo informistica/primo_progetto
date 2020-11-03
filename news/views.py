d72e86a12b2832850c9c8cfe14390b6379196dfb
informistica <mauro.spinarelli@gmail.com>
2020-11-03 19:27:40 +0100

    ...

--- news/views.py
+++ news/views.py
@@ -3,15 +3,13 @@ from .models import Articolo, Giornalista
 # Create your views here.
 
 def home(request):
-     a = []
-     g = []
+     a = ""
+     g = ""
      for art in Articolo.objects.all():
-         a.append(art.titolo)
+         a += (art.titolo + "<br>")
 
      for gio in Giornalista.objects.all():
-         g.append(gio.nome)
-
-     response = str(a) + "<br>" + str(g)
-     print(response)
+         g += (gio.nome + "<br>")
+     response = "Articoli:<br>" + a + "<br>Giornalisti:<br>" + g
 
      return HttpResponse("<h1>" + response + "</h1>")

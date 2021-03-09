ace68b4ea9ca69dd2155b6e07168a390b116b18a
informistica <informistica>
2021-03-09 22:44:37 +0100

    Aggiornata views.py

--- forms_app/views.py
+++ forms_app/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import FormContatto
 
 # Create your views here.
 def contatti(request):
-     form = FormContatto()
-     context = {"form": form}
-     return render(request, "contatto.html", context)
+
+    # Se la richiesta è di tipo POST, allora possiamo processare i dati
+    if request.method == "POST":
+
+        # Creiamo l'istanza del form e la popoliamo con i dati della POST request (processo di "binding")
+        form = FormContatto(request.POST)
+
+        # is_valid() controlla se il form inserito è valido:
+        if form.is_valid():
+            # a questo punto possiamo usare i dati validi!
+            # tenere a mente che cleaned_data["nome_dato"] ci permette di accedere ai dati validati e convertiti in tipi standard di Python
+            print("Il Form è Valido!")
+            print("NOME: ", form.cleaned_data["nome"])
+            print("COGNOME: ", form.cleaned_data["cognome"])
+            print("EMAIL: ", form.cleaned_data["email"])
+            print("CONTENUTO: ", form.cleaned_data["contenuto"])
+
+            # ringrazio l'utente per averci contattato - volendo possiamo effettuare un redirect a una pagina specifica
+            return HttpResponse("<h1>Grazie per averci contattato!</h1>")
+
+    # Se la richiesta HTTP usa il metodo GET o qualsiasi altro metodo, allora creo il form di default vuoto
+    else:
+        form = FormContatto()
+
+    # arriviamo a questo punto se si tratta della prima volta che la pagina viene richiesta(con metodo GET), o se il form non è valido e ha errori
+    context = {"form": form}
+    return render(request, "forms_app/contatto.html", context)
+
--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'news',
     'libreria',
     'forms_app',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [

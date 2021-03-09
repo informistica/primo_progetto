a3dd7acab05d26cbf8c30188f50fa1ecba61ca7d
informistica <informistica>
2021-03-09 18:20:43 +0100

    ...

--- libreria/admin.py
+++ libreria/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Autore, Genere, Libro
 # Register your models here.
 # superuser e password= maurospinarelli
-
+#super user : admin1 admin1
 admin.site.register(Autore)
 admin.site.register(Genere)
 admin.site.register(Libro)

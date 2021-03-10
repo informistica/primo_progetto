3537204a3a5fd1c92fb5fe2f6afd84e98784cfa9
informistica <informistica>
2021-03-10 20:06:39 +0100

    update

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -7,9 +7,18 @@ class FormContatto(forms.ModelForm):
         model = Contatto
         fields = "__all__"
 
-    
-    def clean_contenuto(self):
+        widgets = {
+                    'nome': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+                    'cognome': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+                    'email': forms.EmailInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+                    'contenuto': forms.Textarea(attrs={'placeholder': 'Area testuale scrivi almeno 20 caratteri', 
+                                                       'class': 'form-control'})
+                }
+
+    def clean_contenuto(self): #nome funzione: clean_nomecampodavalidare
         dati = self.cleaned_data["contenuto"]
-        if "parola" in dati:
-            raise ValidationError("Il contenuto inserito viole le norme del sito!")
+        if "parola" in dati:   #parola non è ammesso 
+            raise ValidationError("Il contenuto inserito viola le norme del sito!")
+        if len(dati)<20:
+            raise ValidationError("Il contenuto inserito è troppo breve")
         return dati

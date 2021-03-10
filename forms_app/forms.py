d74bf09c8b5ce23f7aca82c8db74aee9eb146187
informistica <informistica>
2021-03-10 18:25:39 +0100

    update

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Contatto
+from django.core.exceptions import ValidationError
 
 class FormContatto(forms.ModelForm):
     class Meta:
         model = Contatto
         fields = "__all__"
+
+    
+    def clean_contenuto(self):
+        dati = self.cleaned_data["contenuto"]
+        if "parola" in dati:
+            raise ValidationError("Il contenuto inserito viole le norme del sito!")
+        return dati

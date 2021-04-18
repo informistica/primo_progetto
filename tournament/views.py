4a14e60a58cc5fa3a3dbae7e741c1106c79a3af8
informistica <informistica>
2021-04-18 18:28:36 +0200

    ...

--- tournament/views.py
+++ tournament/views.py
@@ -1,12 +1,13 @@
-from courses.models import Course
+from tournament.models import Squadre, Tornei
 from django.shortcuts import render
 def index(request):
-    courses=Course.objects.all()
-    context={'courses':courses}
-    context['custom_theme']=getDefaultCustomize()
-    students={}
-    for course in courses:
-        students2=list(map(lambda x:x.last_name[0].upper()+x.last_name[1:]+" "+x.first_name[0].upper(),course.students.filter(is_staff=False).order_by("last_name","first_name")))
-        students[course.title]=students2
-    context['students']=students
+    #courses=Course.objects.all()
+    tornei=Tornei.objects.all()
+    context={'tornei':tornei}
+
+    squadre={}
+    for torneo in tornei:
+        squadre2=list(map(lambda x:x.nome.upper(),torneo.partecipanti.all().order_by("nome")))
+        squadre[torneo.nome]=squadre2
+    context['squadre']=squadre
     return render(request,"index.html",context)

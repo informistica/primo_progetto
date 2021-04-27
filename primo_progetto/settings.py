3f9163dd8a042ad07fce1763435a94f99572f376
informistica <informistica>
2021-04-27 20:46:10 +0200

    sistemati file statici

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -140,6 +140,6 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, './staticfiles')]  # local
 
 ######
-
-STATIC_ROOT = os.path.join(BASE_DIR, './staticfiles')  #online
+STATICFILES_DIRS = [os.path.join(BASE_DIR, './staticfiles')]
+#STATIC_ROOT = os.path.join(BASE_DIR, './staticfiles')  #online
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'   #online
